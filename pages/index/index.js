1402a69b221d4790be622bbd779132a7ead8cdba
albertdong <albertdong@synnex.com>
2021-04-02 18:06:11 +0800

    add index page

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,28 @@
-// index.js
-// 获取应用实例
-const app = getApp()
+
+const app = getApp();
 
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad() {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
+  uploadPdf() {
+    wx.chooseMessageFile({
+      count: 1,
+      type: 'file',
+      extension: ['pdf'],
+      success(res) {
+        if (res.tempFiles.length) {
+          app.globalData.fileObj = res.tempFiles[0];
+          wx.redirectTo({
+            url: '../exportpdf/index',
           })
         }
-      })
-    }
-  },
-  getUserInfo(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      },
+      fail(res) {
+        wx.showToast({
+          title: 'Upload File Failed!',
+          icon: 'error',
+          duration: 5000,
+        })
+        console.log(res)
+      }
     })
   }
 })
